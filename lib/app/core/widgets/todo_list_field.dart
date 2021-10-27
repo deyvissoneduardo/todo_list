8f4ed77d9746251409ad79846bc60cfcf5272b5e
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2021-10-26 21:26:23 -0300

    custom text form field

---
+++ lib/app/core/widgets/todo_list_field.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/app/core/widgets/todo_list_icon.dart';
+
+class TodoListField extends StatelessWidget {
+  final String label;
+  final bool obscureText;
+  final IconButton? sufixIconButton;
+  late final ValueNotifier<bool> obscureTextVN;
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? formFieldValidator;
+  TodoListField({
+    Key? key,
+    required this.label,
+    this.sufixIconButton,
+    this.obscureText = false,
+    this.controller,
+    this.formFieldValidator,
+  })  : assert(obscureText == true ? sufixIconButton == null : true,
+            'obscureText não pode esta junto com sufixIconButton'),
+        obscureTextVN = ValueNotifier(obscureText),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: obscureTextVN,
+      builder: (_, obscureTextValue, child) {
+        return TextFormField(
+          controller: controller,
+          validator: formFieldValidator,
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: const TextStyle(fontSize: 15, color: Colors.black),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(color: Colors.red),
+            ),
+            isDense: true,
+            suffixIcon: sufixIconButton ??
+                (obscureText == true
+                    ? IconButton(
+                        onPressed: () {
+                          obscureTextVN.value = !obscureTextValue;
+                        },
+                        icon: Icon(
+                          !obscureTextValue
+                              ? TodoListIcon.eye_slash
+                              : TodoListIcon.eye,
+                          size: 15,
+                        ),
+                      )
+                    : null),
+          ),
+          obscureText: obscureTextValue,
+        );
+      },
+    );
+  }
+}
