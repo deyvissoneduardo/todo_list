e39475666fa3c0f19f4d2b2863886ea61a8f20c3
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2021-10-26 21:54:07 -0300

    build layout register

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -2,15 +2,19 @@ import 'package:provider/provider.dart';
 import 'package:todo_list/app/core/modules/todo_list_modules.dart';
 import 'package:todo_list/app/modules/auth/login/login_controller.dart';
 import 'package:todo_list/app/modules/auth/login/login_page.dart';
+import 'package:todo_list/app/modules/auth/register/register_controller.dart';
+import 'package:todo_list/app/modules/auth/register/register_page.dart';
 
 class AuthModule extends TodoListModules {
   AuthModule()
       : super(
           bindings: [
             ChangeNotifierProvider(create: (_) => LoginController()),
+            ChangeNotifierProvider(create: (_) => RegisterController()),
           ],
           routers: {
             '/login': (context) => const LoginPage(),
+            '/register': (context) => const RegisterPage(),
           },
         );
 }
