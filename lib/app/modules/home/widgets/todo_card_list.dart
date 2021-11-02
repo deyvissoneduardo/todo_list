31654cdb7cf8c7ce8d420a0b8b1da32e3ef8e616
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2021-11-02 20:49:08 -0300

    home layout

---
+++ lib/app/modules/home/widgets/todo_card_list.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/app/core/ui/theme_extensions.dart';
+
+class TodoCardList extends StatefulWidget {
+  const TodoCardList({Key? key}) : super(key: key);
+
+  @override
+  State<TodoCardList> createState() => _TodoCardListState();
+}
+
+class _TodoCardListState extends State<TodoCardList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(
+        minHeight: 120,
+        maxWidth: 150,
+      ),
+      margin: const EdgeInsets.only(right: 10),
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: context.primaryColor,
+        border: Border.all(
+          width: 1,
+          color: Colors.grey.withOpacity(.8),
+        ),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '10 TASKS',
+            style: context.titleText.copyWith(
+              fontSize: 10,
+              color: Colors.white,
+            ),
+          ),
+          const Text(
+            'HOJE',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          LinearProgressIndicator(
+            backgroundColor: context.primaryColorLight,
+            valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+            value: 0.4,
+          )
+        ],
+      ),
+    );
+  }
+}
